f49e0b013c2d1c266a587fb2c91cd3f8892fb6fc
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-01-31 08:08:56 -0300

    add initial dependencies to the project and configuring initial routes

---
+++ src/app/pages/upcoming-movies/upcoming-movies.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UpcomingMoviesComponent } from './upcoming-movies.component';
+
+@NgModule({
+  declarations: [UpcomingMoviesComponent],
+  imports: [CommonModule],
+})
+export class UpcomingMoviesModule {}
