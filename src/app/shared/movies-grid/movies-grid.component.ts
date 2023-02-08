02dd4ed7a0a189f0993542acc0ba2e4b7602e8ae
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-02-08 17:11:38 -0300

    customize the movies grid component to show movie posters instead of backdrops

--- src/app/shared/movies-grid/movies-grid.component.ts
+++ src/app/shared/movies-grid/movies-grid.component.ts
@@ -14,25 +14,49 @@ export class MoviesGridComponent implements OnInit {
   @Input() subtitle: string = '';
   @Input() link: string = '';
   @Input() linkTitle: string = '';
-  movieBackdropBaseUrl: string = `${environment.baseImageUrl}w500`;
   moviePosterBaseUrl: string = `${environment.baseImageUrl}w500`;
   posterIndex: number = 0;
-  backdropOneIndex: number = 0;
-  backdropTwoIndex: number = 0;
+  posterOneIndex: number = 0;
+  posterTwoIndex: number = 0;
+  posterThreeIndex: number = 0;
+  posterFourIndex: number = 0;
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    while (
-      this.posterIndex === this.backdropOneIndex ||
-      this.backdropOneIndex === this.backdropTwoIndex ||
-      this.backdropTwoIndex === this.posterIndex
-    ) {
-      this.posterIndex = this.getRandomInt();
-      this.backdropOneIndex = this.getRandomInt();
-      this.backdropTwoIndex = this.getRandomInt();
-    }
+    this.setIndexValues();
   }
   getRandomInt(): number {
-    return Math.floor(Math.random() * 21);
+    return Math.floor(Math.random() * this.movies.length);
+  }
+
+  setIndexValues(): void {
+    while (
+      this.posterIndex === this.posterOneIndex ||
+      this.posterIndex === this.posterTwoIndex ||
+      this.posterIndex === this.posterThreeIndex ||
+      this.posterIndex === this.posterFourIndex ||
+      this.posterOneIndex === this.posterTwoIndex ||
+      this.posterOneIndex === this.posterThreeIndex ||
+      this.posterOneIndex === this.posterFourIndex ||
+      this.posterTwoIndex === this.posterThreeIndex ||
+      this.posterTwoIndex === this.posterFourIndex ||
+      this.posterThreeIndex === this.posterFourIndex
+    ) {
+      this.posterIndex = this.getRandomInt();
+      this.posterOneIndex = this.getRandomInt();
+      this.posterTwoIndex = this.getRandomInt();
+      this.posterThreeIndex = this.getRandomInt();
+      this.posterFourIndex = this.getRandomInt();
+    }
+
+    /*
+    Another way to get dinamic index from 0 to 20
+        const indices = Array.from({ length: 21 }, (_, i) => i);
+    indices.sort(() => Math.random() - 0.5);
+
+    this.posterIndex = indices[0];
+    this.posterOneIndex = indices[1];
+    this.posterTwoIndex = indices[2];
+    this.posterThreeIndex = indices[3];
+    this.posterFourIndex = indices[4];
+    */
   }
 }
