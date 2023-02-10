bb1e9298b5c12395772cc5272e649ea5734ba755
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-02-10 08:37:52 -0300

    inject the new search service and store the data of the movie request service for search query method

--- src/app/services/movie-request.service.ts
+++ src/app/services/movie-request.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { MoviesData } from '../interfaces/movie-data.interface';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { MovieDetail } from '../interfaces/movie-detail.interface';
+import { SearchResultsPageService } from './search-results-page.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MovieRequestService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private searchResults: SearchResultsPageService
+  ) {}
 
   getPopular(page: number = 1): Observable<MoviesData> {
     const url: string = `${environment.baseUrl}/movie/popular?${environment.language}&page=${page}`;
@@ -35,5 +39,10 @@ export class MovieRequestService {
     const url: string = `${environment.baseUrl}/movie/${id}?${environment.language}&append_to_response=videos`;
     return this.http.get<MovieDetail>(url);
   }
-  getSearchResult() {}
+  getSearchResult(query: string): Observable<MoviesData> {
+    const url: string = `${environment.baseUrl}/search/movie?query=${query}`;
+    return this.http
+      .get<MoviesData>(url)
+      .pipe(tap((data) => this.searchResults.setSearchResults(data)));
+  }
 }
