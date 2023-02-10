25990a501b34c7d498a256a495877e63035d3e55
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-02-10 08:41:53 -0300

    add the new searchbox component to the header in desktop

--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { RouterModule } from '@angular/router';
+import { SearchboxModule } from '../searchbox/searchbox.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SearchboxModule],
   exports: [HeaderComponent],
 })
 export class HeaderModule {}
