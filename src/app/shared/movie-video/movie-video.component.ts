6a5139e0d2fe67e8f763974091a96096ab512ceb
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-02-10 08:38:28 -0300

    add a method in the movie vide component to reload the video when the user navigate to another movie detail page

--- src/app/shared/movie-video/movie-video.component.ts
+++ src/app/shared/movie-video/movie-video.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
 import { MovieDetail, Video } from '../../interfaces/movie-detail.interface';
 import { OnInit } from '@angular/core';
 
@@ -7,20 +7,26 @@ import { OnInit } from '@angular/core';
   templateUrl: './movie-video.component.html',
   styleUrls: ['./movie-video.component.scss'],
 })
-export class MovieVideoComponent implements OnInit {
+export class MovieVideoComponent implements OnInit, OnChanges {
   @Input() movie: MovieDetail | null = null;
   videoUrl: string = '';
 
   ngOnInit(): void {
     if (this.movie?.videos?.results?.length) {
-      let videoTrailer =
-        this.movie?.videos?.results?.filter(
-          (video) => video?.type == 'Trailer'
-        )[0] || null;
-      this.videoUrl = this.getVideoUrl(videoTrailer) || '';
+      this.initializeVideo();
     }
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.initializeVideo();
+  }
+  initializeVideo() {
+    let videoTrailer =
+      this.movie?.videos?.results?.filter(
+        (video) => video?.type == 'Trailer'
+      )[0] || null;
+    this.videoUrl = this.getVideoUrl(videoTrailer!) || '';
+  }
   getVideoUrl(video: Video): string {
     if (video.site == 'YouTube') {
       return `https://www.youtube.com/embed/${video.key}?enablejsapi=1&mute=0`;
