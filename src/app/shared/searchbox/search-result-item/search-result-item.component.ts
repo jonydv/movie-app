445c73ddd87a0e289e1bd0fbb44946a44c994b8c
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-02-10 08:41:10 -0300

    add methods to emit an event in the search result item component when the user select a movie and navigate to reset the searchbox component

---
+++ src/app/shared/searchbox/search-result-item/search-result-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { Movie } from '../../../interfaces/movie-data.interface';
+
+@Component({
+  selector: 'app-search-result-item',
+  templateUrl: './search-result-item.component.html',
+  styleUrls: ['./search-result-item.component.scss'],
+})
+export class SearchResultItemComponent implements OnInit {
+  @Input() movie!: Movie;
+  @Output() navigated: EventEmitter<boolean> = new EventEmitter();
+  posterPath: string = '';
+
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.posterPath = `${environment.baseImageUrl}w300/${this.movie.poster_path}`;
+  }
+
+  navigateToMovieDetails(id: string | number): void {
+    this.router
+      .navigateByUrl(`/movie/${id}`)
+      .then((navigated) => this.navigated.emit(navigated));
+  }
+}
