d02d0e7fd808b92caa6142d00d61622a55e55e6b
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-02-22 09:15:26 -0300

    remove the spinner with backdrop of the movie details component

--- src/app/pages/movie-details/movie-details.module.ts
+++ src/app/pages/movie-details/movie-details.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieDetailsComponent } from './movie-details.component';
 import { MovieDetailsRoutingModule } from './movie-details-routing.module';
-import { SpinnerWithBackdropModule } from '../../shared/spinner-with-backdrop/spinner-with-backdrop.module';
 import { MovieVideoModule } from '../../shared/movie-video/movie-video.module';
 import { MoviesGridModule } from '../../shared/movies-grid/movies-grid.module';
 import { MovieInfoDetailsModule } from '../../shared/movie-info-details/movie-info-details.module';
@@ -15,7 +14,6 @@ import { MovieCreditsModule } from '../../shared/movie-credits/movie-credits.mod
   imports: [
     CommonModule,
     MovieDetailsRoutingModule,
-    SpinnerWithBackdropModule,
     MovieVideoModule,
     MoviesGridModule,
     MovieInfoDetailsModule,
