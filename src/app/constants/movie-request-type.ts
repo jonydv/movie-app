29e57b67d4e64d90606c8dff797e42b1b2a6e67b
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-02-23 10:23:04 -0300

    create a movie request type interface and constants to validate the type of request to the back end

---
+++ src/app/constants/movie-request-type.ts
@@ -0,0 +1,9 @@
+import { MovieRequestType } from '../interfaces/movie-request-type.interface';
+
+export const movieRequestType: MovieRequestType = {
+  search: 'SEARCH',
+  upcoming: 'UPCOMING',
+  topRated: 'TOP_RATED',
+  nowPlaying: 'NOW_PLAYING',
+  genre: 'GENRE',
+};
---
+++ src/app/interfaces/movie-request-type.interface.ts
@@ -0,0 +1,7 @@
+export interface MovieRequestType {
+  search: string;
+  upcoming: string;
+  topRated: string;
+  nowPlaying: string;
+  genre: string;
+}
