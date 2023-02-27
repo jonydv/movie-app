688d14da04871403885bed2d764173e052d68ac7
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-02-27 17:44:44 -0300

    add new attributes  and styles to the not-found component with a link to the homepage

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent {
-
+  title: string = 'Oops... the resource was not found';
+  paragraph: string = `Sorry, the page you were looking for is not available`;
+  goHomeText: string = 'Go to the homepage';
+  link: string = '/';
+  imgUrl: string = '../../../assets/images/clapperboard.webp';
+  imgAlt: string = 'Clapperboard vector';
 }
--- src/app/pages/not-found/not-found.module.ts
+++ src/app/pages/not-found/not-found.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found.component';
 import { NotFoundRoutingModule } from './not-found-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [NotFoundComponent],
-  imports: [CommonModule, NotFoundRoutingModule],
+  imports: [CommonModule, NotFoundRoutingModule, RouterModule],
 })
 export class NotFoundModule {}
