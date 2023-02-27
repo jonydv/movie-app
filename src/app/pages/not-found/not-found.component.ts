8b3f1b22d1b21498ecdba602b807e4391f342a86
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-02-27 17:50:19 -0300

    scroll to top when not found component init

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
   title: string = 'Oops... the resource was not found';
   paragraph: string = `Sorry, the page you were looking for is not available`;
   goHomeText: string = 'Go to the homepage';
   link: string = '/';
   imgUrl: string = '../../../assets/images/clapperboard.webp';
   imgAlt: string = 'Clapperboard vector';
+  ngOnInit(): void {
+    window.scrollTo(0, 0);
+  }
 }
