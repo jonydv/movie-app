01d9229044144035aad9fdc993d6714aa2c53a7e
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-03-05 10:46:15 -0300

    add new logic in the movie genres component to add a "x" in the active genres on the movie list, and change the isMobile method to return true in the first call

--- src/app/services/breakpoint.service.ts
+++ src/app/services/breakpoint.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { map, startWith, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -14,15 +14,16 @@ export class BreakpointService {
       '(max-width: 992px)': true,
     },
   };
+  private calls: number = 0;
   constructor(private breakpointObserver: BreakpointObserver) {}
 
   isMobile(): Observable<boolean> {
-    const isSSR = typeof window === 'undefined';
+    this.calls++;
     let observable = this.breakpointObserver
       .observe([this.mobileBreakpoint])
       .pipe(map((state: BreakpointState) => state.matches));
-    if (isSSR) {
-      observable = observable.pipe(startWith(true)); // only add startWith() for server-side rendering
+    if (this.calls == 1) {
+      observable = observable.pipe(startWith(true)); // only add startWith() for first call to this method for server-side rendering purpose
     }
 
     return observable;
