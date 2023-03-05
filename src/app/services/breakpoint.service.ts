3fc1f3383989df2d6221869a374414dbe4414544
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-03-05 10:28:33 -0300

    add a startWith operator in the isMobile method and reestructure it, only for get true the firs time when the server render the page

--- src/app/services/breakpoint.service.ts
+++ src/app/services/breakpoint.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, startWith } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BreakpointService {
   mobileBreakpoint: string = '(max-width: 992px)';
+  initialBreakpointState: BreakpointState = {
+    matches: true,
+    breakpoints: {
+      '(max-width: 992px)': true,
+    },
+  };
   constructor(private breakpointObserver: BreakpointObserver) {}
 
   isMobile(): Observable<boolean> {
-    return this.breakpointObserver
+    const isSSR = typeof window === 'undefined';
+    let observable = this.breakpointObserver
       .observe([this.mobileBreakpoint])
       .pipe(map((state: BreakpointState) => state.matches));
+    if (isSSR) {
+      observable = observable.pipe(startWith(true)); // only add startWith() for server-side rendering
+    }
+
+    return observable;
   }
 }
