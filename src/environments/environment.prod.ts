62e35be1bf6046535952f96980fc1fffa2e8db5e
Jonatan David Villalba <jonatandavidvillalba@gmail.com>
2026-07-08 15:49:39 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   language: 'language=en-US',
   imdbBaseUrl: 'https://www.imdb.com/title/',
   accessToken:
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTE2NmE4NGQzNDkyZTFiYjM4OWU5NDRiYzMzMTNhMyIsInN1YiI6IjVmYWFmMWE4MWMwNDM1MDAzZDM4ZTAzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n3L1jlEkRuPjoFUsLS0uYYYtujGwPYDEl47tR5miwIc',
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NGJjMmNjOTA2YTk5YTcwNDM1N2E1ZDdjZDI2YTFkMiIsIm5iZiI6MTYwNTAzODUwNC41MzksInN1YiI6IjVmYWFmMWE4MWMwNDM1MDAzZDM4ZTAzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M3W-R6ESyHFyLfTFcibR12J0_-CUmDVZ3FFEqKlCpqg',
 };
